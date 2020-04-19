35a540dde9973ca316c6a47e2a8091d31ae785ad
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-04-19 01:22:14 +0100

    Make starter template more interesting

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,26 @@
-/** 
+/**
  * Controls crank-nativescript log verbosity.
  * true: all logs; false: only error logs.
  */
-Object.defineProperty(global, '__DEV__', { value: false });
+Object.defineProperty(global, '__DEV__', { value: true });
 
 import { start } from "crank-native";
 import { createElement } from "@bikeshaving/crank";
 import { StackLayout } from "@nativescript/core";
 import AppContainer from "./components/AppContainer";
 
+const rootView = new StackLayout();
+rootView.width = { value: 100, unit: "%" };
+rootView.height = { value: 100, unit: "%" };
+
 start(
     createElement(
+        //@ts-ignore
         AppContainer,
-        {},
+        {
+            rootView,
+        },
         null
     ) as any, // FIXME: improve typings for this.
-    new StackLayout()
+    rootView
 );
--- app/components/AppContainer.tsx
+++ app/components/AppContainer.tsx
@@ -1,15 +1,55 @@
 /** @jsx createElement */
-import {createElement} from "@bikeshaving/crank";
-import { Color } from "@nativescript/core";
+import {createElement} from "@bikeshaving/crank/cjs/index";
+import { alert } from "tns-core-modules/ui/dialogs";
+import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
+import { Frame } from "@nativescript/core";
 
-export default function Greeting({name="World"}) {
+interface Props {
+    name: string,
+    rootView: Frame,
+}
+
+export default function Greeting({ name = "World", rootView }: Props) {
+    console.log(`AppContainer got rootView:`, rootView);
     return (
-        <contentView
-            backgroundColor={new Color("yellow")}
-            width={{ value: 100, unit: "%" }}
-            height={{ value: 100, unit: "%" }}
-        >
-            <label fontSize={48} paddingLeft={16}>Hello {name}</label>
-        </contentView>
+        <page>
+            <gridLayout
+                width={{ value: 100, unit: "%" }}
+                height={{ value: 100, unit: "%" }}
+                rows={[
+                    new ItemSpec(1, "star"),
+                    new ItemSpec(1, "auto"),
+                    new ItemSpec(1, "auto"),
+                    new ItemSpec(1, "star"),
+                ]}
+                columns={[
+                    new ItemSpec(1, "star"),
+                    new ItemSpec(200, "pixel"),
+                    new ItemSpec(1, "star"),
+                ]}
+            >
+                <label
+                    row={1}
+                    col={1}
+                    className="info"
+                    textAlignment={"center"}
+                    fontSize={24}
+                >
+                    <formattedString>
+                        <span className="fas" text="&#xf135;"/>
+                        <span> Hello {name}</span>
+                    </formattedString>
+                </label>
+                <button
+                    row={2}
+                    col={1}
+                    fontSize={24}
+                    textAlignment={"center"}
+                    onTap={() => alert("Tap received!")}
+                >
+                    Tap me
+                </button>
+            </gridLayout>
+        </page>
     );
 }
